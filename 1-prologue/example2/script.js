6069fa7184e91e766e0667bb9d38aeb71e16aedd
Takumi Kawaguchi <tcgds4@gmail.com>
2021-12-29 23:19:19 +0900

    [update]1章途中まで

---
+++ 1-prologue/example2/script.js
@@ -0,0 +1,6 @@
+var app = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'you loaded this page on ' + new Date().toLocaleDateString()
+    }
+})
---
+++ 1-prologue/example4/script.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: "#app-4",
+    data: {
+        todos: [
+            { text: 'javascript book' },
+            { text: 'python book' },
+            { text: 'C# book' }
+        ]
+    }
+})
