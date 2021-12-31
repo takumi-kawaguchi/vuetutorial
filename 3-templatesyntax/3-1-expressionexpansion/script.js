01a1b0fd40e2bee8afa036e9ced954584b8aa776
Takumi Kawaguchi <tcgds4@gmail.com>
2021-12-31 12:35:44 +0900

    [update]3章終了

---
+++ 3-templatesyntax/3-1-expressionexpansion/script.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        msg: 'this is message',
+        rawHtml: '<span style="color:red;">this should be red.</span>',
+        dynamicID: 'testid',
+        isButtonDisabled: true
+    }
+})
---
+++ 3-templatesyntax/3-2-directive/script.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        seen: true,
+        url: 'https://example.com/'
+    },
+    methods: {
+        test: function () {
+            alert('terererest');
+        }
+    }
+})
