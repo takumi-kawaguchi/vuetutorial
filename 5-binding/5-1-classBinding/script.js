bfc66511cfd86e02d584df32ca9ec0fa8413f6a6
Takumi Kawaguchi <tcgds4@gmail.com>
2022-01-01 23:45:39 +0900

    [update]5章途中まで

---
+++ 5-binding/5-1-classBinding/script.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        isActive: false,
+        hasError: true,
+        classObject: {
+            test: true,
+            error: true
+        }
+    }
+})
