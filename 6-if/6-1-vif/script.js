6904bfc8a874e38a6bc8f2a23a2657ba80ce0f08
Takumi Kawaguchi <tcgds4@gmail.com>
2022-01-05 21:49:50 +0900

    [update]6章終了

--- 6-if/6-1-vif/script.js
+++ 6-if/6-1-vif/script.js
@@ -1,6 +1,18 @@
 var app = new Vue({
     el: '#app',
     data: {
-        awesome: true
+        awesome: true,
+        type: 'A',
+        loginType: 'username',
+        ok: true
+    },
+    methods: {
+        toggleLoginType: function () {
+            if (this.loginType === 'username') {
+                this.loginType = 'email'
+            } else {
+                this.loginType = 'username'
+            }
+        }
     }
 })
