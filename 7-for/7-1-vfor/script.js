0f56305c7d773ef7ea40508ceba96f05b50ab68c
Takumi Kawaguchi <tcgds4@gmail.com>
2022-01-10 22:37:29 +0900

    [update]7章終了

--- 7-for/7-1-vfor/script.js
+++ 7-for/7-1-vfor/script.js
@@ -5,10 +5,18 @@ var app = new Vue({
             { message: 'Foo' },
             { message: 'Bar' }
         ],
+        numbers: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
         object: {
             title: 'how to do lists in vue',
             author: 'jane doe',
             publishedAt: '2016-04-10'
+        }
+    },
+    computed: {
+        evenNumbers: function () {
+            return this.numbers.filter(function (number) {
+                return number % 2 === 0
+            })
         }
     }
 })
