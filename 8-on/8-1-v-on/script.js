db6c1802e23afb0567b2e0e1436ffe9895ef3f55
Takumi Kawaguchi <tcgds4@gmail.com>
2022-01-11 23:17:35 +0900

    [update]8章途中

---
+++ 8-on/8-1-v-on/script.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        counter: 0,
+        name: 'Vue.js'
+    },
+    methods: {
+        greet: function (event) {
+            alert('hello ' + this.name + '!')
+            if (event) {
+                alert(event.target.tagName)
+            }
+        },
+        say: function (message) {
+            alert(message)
+        }
+    }
+})
