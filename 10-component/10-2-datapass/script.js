73c4a11e1d8565278fd984f77dba98b9d0061f85
Takumi Kawaguchi <tcgds4@gmail.com>
2022-01-19 22:07:43 +0900

    [update]10章途中

--- 10-component/10-2-datapass/script.js
+++ 10-component/10-2-datapass/script.js
@@ -3,13 +3,41 @@ Vue.component('blog-post', {
     template: '<h3>{{ title }}</h3>'
 })
 
+Vue.component('refactor-blog-post', {
+    props: ['post'],
+    template: `
+        <div class="blog-post">
+            <h3>{{ post.title }} - {{ post.id }}</h3>
+            <p v-html="post.content"></p>
+        </div>
+    `
+})
+
+Vue.component('childcomponent-blog-post', {
+    props: ['post'],
+    template: `
+        <div class="blog-post">
+            <h3>{{ post.title }}</h3>
+            <button v-on:click="$emit('enlarge-text', 0.1)">enlarge text</button>
+            <div v-html="post.content"></div>
+        </div>
+    `
+})
+
 new Vue({
     el: '#datapass',
     data: {
         posts: [
-            { id: 1, title: 'my vue' },
-            { id: 2, title: 'dairy life' },
-            { id: 3, title: 'dinner' }
-        ]
+            { id: 1, title: 'my vue', content: 'vue is very good!' },
+            { id: 2, title: 'dairy life', content: 'good day it was!' },
+            { id: 3, title: 'dinner', content: 'yammy!' }
+        ],
+        postFontSize: 1
+    },
+    methods: {
+        onEnlargeText: function (enlargeAmount) {
+            this.postFontSize += enlargeAmount
+            console.log(enlargeAmount)
+        }
     }
 })
